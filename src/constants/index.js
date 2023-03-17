a14f5165be21a4717eb4e060205f9772f16b010b
Rishabh <rishabhravimurthy72@gmail.com>
2023-03-18 03:41:40 +0800

    First Final Version

---
+++ src/constants/index.js
@@ -0,0 +1,289 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Python Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend and Frontend Developer",
+      icon: backend,
+    },
+    {
+      title: "Javascript user",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Fun and Progressive",
+      company_name: "general",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "2018",
+      points: [
+        "Helping children build shelter, toilets, playgrounds, schools as well as teaching them foreign languages like English along with a little bit of Math.",
+        "Developed my python skills and made many projects like a number guessing game or a simple chat bot.",
+        "Got selected for the school football team. Made four goals the entire season.",
+        "This was the year I travlled to America with my family and learnt about many univerisities and opportunities.",
+      ],
+    },
+    {
+      title: "The So Called Downfall",
+      company_name: "Covid",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "2019",
+      points: [
+        "Covid hit the planet and killed many people - very sad and unfortunate.",
+        "went to Cambodia before covid started luckily and did a lot of community service as well as learnt about culture.",
+        "Improved coding skills and learnt javascript while still continuing python. Also got very good at web development.",
+        "Participating in code reviews and providing constructive feedback to others.",
+      ],
+    },
+    {
+      title: "The New Meta",
+      company_name: "The Big One",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "2022",
+      points: [
+        "Developing and maintaining web applications using React and other coding languages.",
+        "Got a grade 9 in maths IGCSE.",
+        "Implementing responsive design and stepped up my coding with ensuring cross browser and platform compatibility.",
+        "Started doing photography.",
+      ],
+    },
+    {
+      title: "Developer - Quite Often",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Use knowledge of coding to try and solve in real life problems like converters online, widgets for quick access, ad blockers, etc.",
+        "Got a gold award in the UKMT maths challenge.",
+        "Finished mock exams and GCSE coming up in June.",
+        "Gym is far more frequent now and far more consistant - learnt to be discipline.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Place Holder of Vouch.",
+      name: "Name",
+      designation: "type of person.",
+      company: "Acme Co",
+      image: "https://i.pinimg.com/originals/20/90/0d/20900db3ae3168501923c2335abc7d41.jpg",
+    },
+    {
+      testimonial:
+        "Place Holder of Vouch.",
+      name: "Name",
+      designation: "type of person.",
+      company: "from where.",
+      image: "https://aniyuki.com/wp-content/uploads/2022/03/aniyuki-cute-anime-avatar-profile-picture-12.jpg",
+    },
+    {
+      testimonial:
+        "Place Holder of Vouch.",
+      name: "Name",
+      designation: "type of person.",
+      company: "from where.",
+      image: "https://www.animeinformer.com/wp-content/uploads/2022/09/girl-anime-profile-hd-1024x1024.png",
+    },
+    {
+      testimonial:
+        "Place Holder of Vouch.",
+      name: "Name",
+      designation: "type of person.",
+      company: "from where.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ89TuSxcrGI3eltlXXFTqw_VHTA7W1vFOSQ",
+    },
+    {
+      testimonial:
+        "Place Holder of Vouch.",
+      name: "Name",
+      designation: "type of person.",
+      company: "from where.",
+      image: "https://i.pinimg.com/736x/80/3d/9d/803d9df3f128eed08db2a58f6b6d6659.jpg",
+    },
+    {
+      testimonial:
+        "Place Holder of vouch.",
+      name: "Name",
+      designation: "type of person",
+      company: "from where.",
+      image: "https://aniyuki.com/wp-content/uploads/2022/03/aniyuki-cute-anime-avatar-profile-picture-17.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Place Holder",
+      description:
+        "Place Holder",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Place Holder 2",
+      description:
+        "Place Holder",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Place Holder 3",
+      description:
+        "Place Holder",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
